0d4a28df573a8c932489bde1cf8a0ae6616e052a
shafucheng <flynn.sha@outlook.com>
2021-11-30 12:35:35 +0800

    use @vueuse/router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+import { useRouteQuery } from '@vueuse/router'
+
+const routeQuery = reactive({
+  name: useRouteQuery('name'),
+  page: useRouteQuery('page'),
+})
+</script>
+
 <template>
   <a-space>
     <router-link to="/">Home</router-link>
     <router-link to="/other">Other</router-link>
   </a-space>
+  <div>{{ routeQuery }}</div>
   <router-view />
 </template>
