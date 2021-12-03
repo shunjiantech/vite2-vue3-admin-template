b681f87939d591dae33b4cdf3e5e10d6dbe98643
shafucheng <flynn.sha@outlook.com>
2021-12-03 16:37:19 +0800

    feat: Dayjs组件支持空值转换

--- src/components/Dayjs.vue
+++ src/components/Dayjs.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import dayjs, { Dayjs } from 'dayjs'
 
+type ValueType = number | string | null
+
 const props = defineProps<{
-  value: number | string
+  value?: ValueType
   format?: string
 }>()
 
 const emit = defineEmits<{
-  (e: 'update:value', value: number | string): void
+  (e: 'update:value', value?: ValueType): void
 }>()
 
 const value = useVModel(props, 'value', emit)
 
-function handleUpdate(newDayjsObj: Dayjs) {
+const valueComputed = computed(() => {
+  if (value.value === null || value.value === undefined) {
+    // 讲 null 强制识别为 undefined
+    // 因为antdv的date-picker value不支持null但是点击清空按钮update:value事件返回的却是null
+    return <undefined>value.value
+  }
+  return dayjs(value.value)
+})
+
+function handleUpdate(newDayjsObj?: Dayjs | null) {
+  if (newDayjsObj === null || newDayjsObj === undefined) {
+    value.value = newDayjsObj
+    return
+  }
   if (props.format) {
     value.value = newDayjsObj.format(props.format)
   } else {
@@ -23,7 +38,7 @@ function handleUpdate(newDayjsObj: Dayjs) {
 
 <template>
   <slot
-    :value="dayjs(value)"
+    :value="valueComputed"
     :update="handleUpdate"
   />
 </template>
